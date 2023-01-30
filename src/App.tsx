a6220b05333374ab54626a7698f7a97d8de1aefa
e.shkriada <e.shkriada@intch.org>
2023-01-30 13:48:38 +0700

    Added the main component

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import { Wrapper } from './styles';
+
+export const App = () => {
+  const tasks = [
+    {
+      id: 1,
+      title: 'Clean the room',
+      description: undefined,
+      date: new Date(),
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: 'Buy dog food',
+      description: 'Treats & Dental Sticks',
+      date: new Date(),
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: 'Call Mom',
+      description: 'After 6pm',
+      date: new Date(),
+      isDone: false,
+    },
+  ];
+
+  return (
+    <Wrapper>
+      <Container>
+        {/* Header */}
+        {/* TasksList */}
+      </Container>
+    </Wrapper>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
 const rootNode = document.getElementById('root');
 
 rootNode &&
   ReactDOM.createRoot(rootNode).render(
     <React.StrictMode>
-      <h1>TO DO List</h1>
+      <App />
     </React.StrictMode>
   );
