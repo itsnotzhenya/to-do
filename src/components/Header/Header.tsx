5a6f318b6a2645972272ffecf7361e3223a7cc72
e.shkriada <e.shkriada@intch.org>
2023-02-07 18:49:23 +0700

    Show menu on Settings icon click

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,66 @@
 import React from 'react';
-import { IconButton } from '@mui/material';
+import { AppBar, IconButton, Menu, MenuItem, Toolbar } from '@mui/material';
 import { SettingsIcon } from '../Icons/SettingsIcon';
 import { Text } from '../Text';
+import { Switch } from '../Switch';
 import { TextType } from '../../types';
-import { Header as StyledHeader } from './styles';
 
 export const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <StyledHeader>
-      <Text content="To Do" type={TextType.PRIMARY_TEXT} />
-      <IconButton aria-label="Settings">
-        <SettingsIcon />
-      </IconButton>
-    </StyledHeader>
+    <AppBar position="static" elevation={0} style={{ background: '#222222' }}>
+      <Toolbar>
+        <Text content="To Do" type={TextType.PRIMARY_TEXT} />
+
+        <div>
+          <IconButton
+            size="large"
+            aria-label="Settings"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit">
+            <SettingsIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            sx={{
+              '.MuiMenu-paper': {
+                backgroundColor: '#282828',
+              },
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}>
+            <MenuItem>
+              <Text content="Show news" type={TextType.SECONDARY_TEXT} />
+              <Switch
+                sx={{ m: 1 }}
+                checked={false}
+                onChange={() => console.log('update')}
+              />
+            </MenuItem>
+          </Menu>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
