6f84511b3496bfb9190ab463f273d81ce23367dc
e.shkriada <e.shkriada@intch.org>
2023-02-08 12:25:45 +0700

    Moved getting news in NewsContext

--- src/components/NewsContextProvider/NewsContextProvider.tsx
+++ src/components/NewsContextProvider/NewsContextProvider.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useState, useCallback } from 'react';
+import React, { createContext, useState, useCallback, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { getter } from '../../utils';
 
 type Props = {
   children: JSX.Element;
@@ -14,8 +16,21 @@ export const NewsContext = createContext<NewsContextType>({
   changeNewsDisplay: () => {},
 });
 
+const TOP_NEWS_URL =
+  'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
+
+const GET_STORY_URL = (id: string | number) =>
+  `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
+
 export const NewsContextProvider = ({ children }: Props) => {
   const [isNewsShown, setIsNewsShown] = useState(true);
+
+  const topStoriesQuery = useQuery('topnews', getter(TOP_NEWS_URL));
+
+  const storyUrl = GET_STORY_URL(topStoriesQuery.data?.[0] ?? '1');
+  const { data } = useQuery('story', getter(storyUrl), {
+    enabled: !topStoriesQuery.isFetching,
+  });
 
   const changeNewsDisplay = useCallback(
     () => setIsNewsShown(!isNewsShown),
--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -1,33 +1,19 @@
 import React, { useContext } from 'react';
-import { useQuery } from 'react-query';
+
 import { NewsContext } from '../NewsContextProvider';
-import { getter } from '../../utils';
 import { useStyles } from './styles';
 
-const TOP_NEWS_URL =
-  'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
-
-const GET_STORY_URL = (id: string | number) =>
-  `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
-
 export const Ticker = () => {
-  const topStoriesQuery = useQuery('topnews', getter(TOP_NEWS_URL));
-
-  const storyUrl = GET_STORY_URL(topStoriesQuery.data?.[0] ?? '1');
-  const { data } = useQuery('story', getter(storyUrl), {
-    enabled: !topStoriesQuery.isFetching,
-  });
-
   const { isNewsShown } = useContext(NewsContext);
 
   const classes: any = useStyles();
-  const text =
+  const fallbackText =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ante elit, faucibus et pellentesque id, aliquam a augue. Proin scelerisque nisl vel fringilla vulputate.';
 
   return isNewsShown ? (
     <div className={classes.tickerWrapper}>
       <div className={classes.ticker}>
-        <div className={classes.text}>{data && data.title}</div>
+        <div className={classes.text}>{fallbackText}</div>
       </div>
     </div>
   ) : null;
